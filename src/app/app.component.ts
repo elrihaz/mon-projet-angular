9bae5ed254e099a647ff5fe3488ce71fba1ae9bd
elrihaz <elrihaz@gmail.com>
2018-06-10 18:46:34 +0200

    dble bind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
-import {
-  Component
-} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector:           'app-root',
   templateUrl:        './app.component.html',
   styleUrls:          ['./app.component.scss']
 })
+
 export class AppComponent {
 
   isAuth            = false;
+
+  appareilOne:        string = 'Machine à laver';
+  appareilTwo:        string = 'Frigo';
+  appareilThree:      string = 'Ordinateur';
 
   constructor() {
     setTimeout(
--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector:       'app-appareil',
-  templateUrl:    './appareil.component.html',
-  styleUrls:      ['./appareil.component.scss']
+  selector:              'app-appareil',
+  templateUrl:           './appareil.component.html',
+  styleUrls:             ['./appareil.component.scss']
 })
 export class AppareilComponent implements OnInit {
 
-  appareilName:   'Machine à laver';
-  appareilStatus: 'éteint';
-
+  @Input() appareilName: string;
+  @Input() appareilStatus: string;
+ 
   constructor() { }
 
   ngOnInit() {
