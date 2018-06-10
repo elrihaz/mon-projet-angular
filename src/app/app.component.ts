6bf81ad26cd00c8bbdda5888d66f718a32419b10
elrihaz <elrihaz@gmail.com>
2018-06-10 17:15:58 +0200

    binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector:           'app-root',
+  templateUrl:        './app.component.html',
+  styleUrls:          ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  isAuth            = false;
+
+  constructor() {
+    setTimeout(
+      ()           => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+  }
+
 }
